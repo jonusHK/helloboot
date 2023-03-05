c996716fcc01bf116f2075b18ca4b54004b7e3b0
jonusHK <positipman@gmail.com>
2023-03-05 18:40:29 +0900

    Update: MySpringBootApplication 어노테이션 추가

---
+++ src/main/kotlin/tobyspring/helloboot/MySpringBootAnnotation.kt
@@ -0,0 +1,11 @@
+package tobyspring.helloboot
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE)
+@Configuration
+@ComponentScan
+annotation class MySpringBootAnnotation
