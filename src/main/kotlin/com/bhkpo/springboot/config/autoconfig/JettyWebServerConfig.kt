aca264f78916aba475fabd388e5c2c72f88e275f
jonusHK <positipman@gmail.com>
2023-03-06 17:35:56 +0900

    Add: 사용자 구성정보로 톰캣 서블릿 웹서버 설정 (+ 자동 구성정보로 서블릿 웹서버 비활성화)

--- src/main/kotlin/com/bhkpo/springboot/config/autoconfig/JettyWebServerConfig.kt
+++ src/main/kotlin/com/bhkpo/springboot/config/autoconfig/JettyWebServerConfig.kt
@@ -2,6 +2,7 @@ package com.bhkpo.springboot.config.autoconfig
 
 import com.bhkpo.springboot.config.ConditionalMyOnClass
 import com.bhkpo.springboot.config.MyAutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Bean
 class JettyWebServerConfig {
 
     @Bean("jettyWebServerFactory")
+    @ConditionalOnMissingBean
     fun servletWebServerFactory(): ServletWebServerFactory {
         return JettyServletWebServerFactory()
     }
