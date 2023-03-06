3569cb9a3c42f7d227d6378f7bb3b403a2626244
jonusHK <positipman@gmail.com>
2023-03-06 16:26:56 +0900

    Add: Contional 어노테이션 사용한 테스트코드

---
+++ src/test/kotlin/com/bhkpo/springboot/study/ConditionalTest.kt
@@ -0,0 +1,65 @@
+package com.bhkpo.springboot.study
+
+import io.kotest.core.spec.style.AnnotationSpec
+import org.assertj.core.api.Assertions.assertThat
+import org.springframework.boot.test.context.runner.ApplicationContextRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Condition
+import org.springframework.context.annotation.ConditionContext
+import org.springframework.context.annotation.Conditional
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.type.AnnotatedTypeMetadata
+
+class ConditionalTest: AnnotationSpec() {
+    @Test
+    fun conditional() {
+        // true
+        ApplicationContextRunner().withUserConfiguration(Config1::class.java).run { context ->
+            assertThat(context).hasSingleBean(MyBean::class.java)
+            assertThat(context).hasSingleBean(Config1::class.java)
+        }
+
+        // false
+        ApplicationContextRunner().withUserConfiguration(Config2::class.java).run { context ->
+            assertThat(context).doesNotHaveBean(MyBean::class.java)
+            assertThat(context).doesNotHaveBean(Config2::class.java)
+        }
+    }
+
+    companion object {
+        @Retention(AnnotationRetention.RUNTIME)
+        @Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
+        @Conditional(BooleanCondition::class)
+        annotation class BooleanConditional(val value: Boolean)
+
+
+        @Configuration
+        @BooleanConditional(true)
+        class Config1 {
+            @Bean
+            fun myBean(): MyBean {
+                return MyBean()
+            }
+        }
+
+        @Configuration
+        @BooleanConditional(false)
+        class Config2 {
+            @Bean
+            fun myBean(): MyBean {
+                return MyBean()
+            }
+        }
+
+        class MyBean {}
+
+        class BooleanCondition: Condition {
+            override fun matches(conditionContext: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
+                val annotationAttributes: MutableMap<String, Any>? =
+                    metadata.getAnnotationAttributes(BooleanConditional::class.java.name)
+                return annotationAttributes?.get("value") as Boolean
+            }
+        }
+
+    }
+}
