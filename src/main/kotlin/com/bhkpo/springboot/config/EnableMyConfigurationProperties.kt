7908c25d9cd0d33c641b5717ca83b18ec551c125
jonusHK <positipman@gmail.com>
2023-03-07 19:45:17 +0900

    Remove: 스프링부트 설정 정보로 전환 위한 커스텀 설정 정보 제거

--- src/main/kotlin/com/bhkpo/springboot/config/EnableMyConfigurationProperties.kt
+++
@@ -1,9 +0,0 @@
-package com.bhkpo.springboot.config
-
-import org.springframework.context.annotation.Import
-import kotlin.reflect.KClass
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
-@Import(MyConfigurationPropertiesImportSelect::class)
-annotation class EnableMyConfigurationProperties(val value: KClass<*>)
--- src/main/kotlin/com/bhkpo/springboot/config/autoconfig/ServerProperties.kt
+++
@@ -1,9 +0,0 @@
-package com.bhkpo.springboot.config.autoconfig
-
-import com.bhkpo.springboot.config.MyConfigurationProperties
-
-@MyConfigurationProperties(prefix = "server")
-class ServerProperties {
-    lateinit var contextPath: String
-    var port: Int? = null
-}
