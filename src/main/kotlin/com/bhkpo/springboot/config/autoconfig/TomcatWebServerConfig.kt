8af2689cbd5bf6ca48e008f2d535fb75bd6b4f39
jonusHK <positipman@gmail.com>
2023-03-07 13:35:27 +0900

    Update: 커스텀 어노테이션을 이용하여 Import 처리

--- src/main/kotlin/com/bhkpo/springboot/config/autoconfig/TomcatWebServerConfig.kt
+++ src/main/kotlin/com/bhkpo/springboot/config/autoconfig/TomcatWebServerConfig.kt
@@ -1,17 +1,17 @@
 package com.bhkpo.springboot.config.autoconfig
 
 import com.bhkpo.springboot.config.ConditionalMyOnClass
+import com.bhkpo.springboot.config.EnableMyConfigurationProperties
 import com.bhkpo.springboot.config.MyAutoConfiguration
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Import
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
-@Import(ServerProperties::class)
+@EnableMyConfigurationProperties(ServerProperties::class)
 class TomcatWebServerConfig {
 
     @Value("\${server.contextPath}")
